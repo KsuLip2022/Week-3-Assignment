660f8114e51e5a3a879f1096c991065cd4a24ed2
Ksenia Dubrovskaya <108101241+KsuLip2022@users.noreply.github.com>
2022-07-16 20:07:41 -0400

    Add files via upload

---
+++ Javaloop.java
@@ -0,0 +1,376 @@
+
+public class Javaloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          //   for (int i = 10; i < 20; i ++) {
+           	//		System.out.println("Value of i = " + i);
+//	}
+		
+		        // int j = 2;
+		      // do {
+                //   System.out.println("Value of j is " + j);
+		         //  j++;
+            // }	
+		    //   while (j>=8);
+          //       	
+             
+	//	int i = 0;
+	//	do {
+	//	  System.out.println(i);
+	//	  i++;
+	//	}
+	//	while (i < 5);
+	
+		
+              //  int i = 0;
+              //  while (i < 5) {
+             //     System.out.println(i);
+             //     i++;
+             //   }  
+                
+              //  int num1 = 20;
+              //  int num2 = 15;
+              //  int num3 = 17;
+              //  if (num1 > num2 && num2 < num3 && num1 > num3) {
+               // 	System.out.println(true);
+            //    }else {
+              //  	System.out.println(false);
+             //   }
+		
+              //  System.out.println(false && (!true && !true));
+		
+		
+	//	double assignment1, assignment2, assignment3, assignment4;
+	//	assignment1 = 80.50;
+	//	assignment2 = 79.30;
+	//	assignment3 = 95.90;
+	//	assignment4 = 100.00;
+		
+	//	     if ((assignment1 + assignment2) > 150.0 && (assignment3 > 70.0 || assignment4 > 70.0)) {
+	//	 System.out.println("You passed the class.");
+	//} else {
+	//  System.out.println("You failed the class.");
+	//    }
+			
+	//	int num1 = 10;
+	//	int num2 = 20;
+	//	int num3 = 25;
+	//	System.out.println((num3<num2) && (num1+num2 > num3));
+		
+		
+		
+	//	int moneyInBank = 1000;
+	//	int rentDue = 500;
+	//	int carPayment = 250;
+	//	int food = 125;
+		
+	//	       if (moneyInBank > (rentDue + carPayment + food)) {
+	//	System.out.println("You have enough money this week.");
+	//	       } else {
+	//	System.out.println("You may need to borrow some money.");
+	//	       }
+		
+		
+	//	System.out.println(!(10>5)||(5<1));
+		
+		
+	//	double sundayTemp = 79.88;
+	//	double mondayTemp = 80.11;
+	//	double tuesdayTemp = 82.11;
+		
+	//	if ( (sundayTemp > mondayTemp) && (sundayTemp > tuesdayTemp)) {
+	//	    System.out.println("Sunday");
+	//	} else if ((mondayTemp > sundayTemp) && (mondayTemp > tuesdayTemp)) {
+	//	    System.out.println("Monday");
+	//	} else {
+	//	    System.out.println("Tuesday");
+	//	} 
+	//	int numericGrade = 50;
+	//	if (numericGrade >= 90) {
+	//		System.out.println("A");
+	//	} else if  (numericGrade >= 80 && numericGrade < 90){
+	//		System.out.println("B");
+	//	} else if (numericGrade >= 70 && numericGrade < 80) {
+	//		System.out.println("C");
+	//	} else if (numericGrade >= 60 && numericGrade < 70) {
+	//		System.out.println("D");
+	//	} else {
+	//		System.out.println("F");
+	//	}
+	//	int num = 70;
+	//	if (num < 100) {
+    //          System.out.println("number is less than 100");
+	//	if (num > 50) {
+	//		  System.out.println("number is greater than 50");
+	//	}
+	//	}	
+	//String randomWord = "small";
+	//if (randomWord.length() >= 10) {
+	//	System.out.println("That is a really long word!");
+	//} else {
+	//	System.out.println("Your word is not that long.");
+	//}
+	//	double a = 495;
+	//	double b = 243;
+	//	if ( a*b % 2 == 0) {
+	//		System.out.println("Even number");
+	//	} else {
+	//		System.out.println("Odd number");
+	//	}
+	//      int monthNumber = 15;
+	      
+	//	switch (monthNumber) {
+	//	case 1: monthNumber = 1;
+	//	System.out.println("January");
+	//	break;
+	//	case 2: monthNumber = 2;
+	//	System.out.println("February");
+	//	break;
+	//	case 3: monthNumber = 3;
+	//	System.out.println("March");
+	//	break;
+	//	case 4: monthNumber = 4;
+	//	System.out.println("April");
+	//	break;
+	//	case 5: monthNumber = 5;
+	//	System.out.println("May");
+	//	break;
+	//	case 6: monthNumber = 6;
+	//	System.out.println("June");
+	//	break;	
+	//	case 7: monthNumber = 7;
+	//	System.out.println("July");
+	//	break;
+	//	case 8: monthNumber = 8;
+	//	System.out.println("Augest");
+	//	break;         
+	//    case 9: monthNumber = 9;
+	//    System.out.println("September");
+	//    break;
+	//    case 10: monthNumber = 10;
+	//    System.out.println("October");
+	//    break;
+	//    case 11: monthNumber = 11;
+	//    System.out.println("November");
+	//	break;
+	//	case 12: monthNumber = 12;
+	//	System.out.println("December");
+	//	break;
+	//	default:
+	//		System.out.println("no ");
+	//	}
+	//	Create a do while loop that checks the length of userWord
+	//	and if that word is less than 5 characters, then print userWord
+	//	to the console 1 time. If it is 5 or more characters print userWord
+	// the console userWord.length() number of times. 
+	//	(i.e. if userWord = "Loops" - then "Loops" would print 5 times, 
+	//	if it is "Java" then it would only print 1 time)
+		
+	//	String userWord = "Javaa";
+	//	int wordLength = userWord.length();
+	//	int counter = wordLength;
+		 
+		
+	//	do {
+	//	    System.out.println(userWord);
+		
+	//	    if ( wordLength < 5 ) {
+	//	        break;
+	//	    } 
+	//	    counter--;
+	//	} while ( counter > 0 );
+		
+		
+	//   System.out.println("Multiplication Table of " + num);
+	//	int num;
+	// j = 1;
+		
+	//   for (num=1; j<=10; j++ ) {
+		  
+		
+	 //  System.out.println(num + "*" + j + "=" + (num*j) );
+	//   }
+	//	int x = 0;
+	//	do {
+	//		System.out.println("I will print!");
+	//		x++;
+	//	} while (x>1);
+		
+		//my own code
+	//	System.out.println("**Origin**");
+	//	int[] arr = { 5, 10, 44, -4, 10 };
+	//	for (int ar=0; ar<arr.length; ar++) {
+	//		System.out.println(arr[ar]);
+	//	}
+	//	System.out.println("**New Array**");
+	//	int x = -8;
+	//	for (int ar: arr) { 
+	//		System.out.println(ar+x);
+	//	}
+		//OpenClass code
+		//	System.out.println("**Original**");
+		//for ( int i = 0; i < arr.length; i++ ) {
+		//    System.out.println(arr[i]);
+		//    arr[i] += x;
+		//}
+		//System.out.println("**New Array**");
+		//for ( int j = 0; j < arr.length; j++ ) {
+		//    System.out.println(arr[j]);
+		//}
+	    //}
+		
+		//      System.out.println("Average value " + (3+9+23+64+2+8+28+93)/8);  	
+        //      int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
+        //              System.out.println(ages[ages.length-1] - ages[0]);		
+		//      int[] ages2 = {3, 9, 23, 64, 2, 8, 28, 93, 6};
+		//              System.out.println(ages2[ages2.length-1] - ages2[0]);  		              
+		//      double sum= 0;
+		//           for(int age: ages) {
+		//            sum += age;	 
+	    //      }
+		//     double average = sum / ages.length;
+		//              System.out.println("Average age " + average);
+		          
+	              	              
+//		 String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+//		 double sumOfLetters = 0;
+//		   for(String name: names) {
+//			   sumOfLetters+=name.length();
+//		}
+//		   System.out.println("The sum of all litters is " + sumOfLetters);
+	//	 System.out.println("Average numbers of litters per name " + sumOfLetters/names.length);	
+//		 for(String name: names) {
+	//		 System.out.print(name+ " ");	 
+	//	}
+		
+	//	  int[] array = {3, 9, 28, 64, 32, 8, 25, 7};
+	//	  System.out.println("The last element of any array is " + array[array.length-1]);
+	//	  System.out.println("The first element of any array is " + array[0]);
+		  
+	//	  int[] nameLengths = {25, 4, 48};
+	//			  double sum = 0;
+	//		           for(int nameLength: nameLengths) {
+	//	            sum += nameLength;	 
+	//		           }
+	//		           double averSum = sum + sumOfLetters;
+	//		           System.out.println("The sum of nameLength array is " + sum);
+	//		           System.out.println("The previously created names array and add the length of "
+	//		           		+ "each name to the nameLengths array is " + averSum);
+			           
+			
+//			System.out.println(sayHello ("Hello"));          
+         
+              
+//     greets("Tom");
+//    greets("Sally");
+   
+	
+//	}
+//	 public static void greets(String name) {
+//		 System.out.println("Hello, " + name + "!");
+//	 }
+	
+	
+// public static String greet(String name) {
+// return("Hi, " + name + "!");
+		
+		
+//   System.out.println(multiGreeting("Hello", 3));     
+//	 }
+//	 public static String multiGreeting(String word, int num) {
+//	 String result = " ";	 
+//	 for (int i= 0; i < num; i++) {
+//		result += word;	 
+//	}
+//	  return result;
+	
+//	 System.out.println(greet("Nick", "Black"));   
+//		}	
+//      public static String greet(String firstName, String lastName) {
+//      return(firstName + " " + lastName); 
+		
+		
+		//double[] array1 = {5, 8, 4, 13, 1};
+		//double[] array2 = {3, 8, 4, 13, 60};
+		//boolean averArray = averageSum(array1) > averageSum(array2);
+		//System.out.println(averArray);
+		
+		
+		//System.out.println(willBuyDrink(true, 10.51));
+//		 int[] arrays = {5, 8, 4, 13, 71};
+//		 int sum = arrayTrue(arrays);
+//		 boolean greaterHundred = sum > 100;
+//		 System.out.println(greaterHundred);
+		
+		
+		
+		
+		String[] textArray = {"one", "two", "three", "four", "five", "six"};
+		int num = 58;
+		//for (String text: textArray) {
+		//	int count = 0;
+			
+		System.out.println(textArray.length);
+		}
+	    	
+	//    public static int[] countCharacters(String[] textArray) {
+	//	int[] result = new int[textArray.length];
+	//	for(int i = 0; i < textArray.length; i++) {
+	//		result [i] = textArray[i].length();
+	//	}
+	//	return result;
+	//     }
+		
+	
+	public static int countCharacters(String[] textArray, int num) {
+	   int count = 0;
+	   for (String text: textArray) {
+		   count += text.length();
+			   
+		 }
+	   return count;
+	  }
+	
+	
+	
+	
+//	  public static int arrayTrue(int[] sumGreater) {
+//	  int sum = 0;
+//	  for (int array: sumGreater) {
+//		  sum += array;    
+//	  }	  
+//	  return sum;
+
+//public static double averageSum(double[] numbers) {
+//	double sum = 0;
+//	for(double number: numbers) {
+//		sum += number;	
+//	}
+//	return sum/numbers.length;
+//}
+
+//public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) { 
+//	if (isHotOutside == true && moneyInPocket > 10.50) {
+//		return true;
+//	}
+//return isHotOutside == true && moneyInPocket > 10.50;
+//}
+
+
+//public int calSum(){
+//	 int sum = 0;
+//	 for (int value : numbers) {
+//	     sum += value;
+//	}
+//	    return sum;
+	
+		
+			
+	 
+	
+	
+	
+	
+	
+	 }	
